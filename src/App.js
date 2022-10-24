afdc5ebdf8fb466acba2ddf9b501ae9593d757ca
LoopMe-Ore <oreoluwa.yusuf@loopme.com>
2022-10-24 19:54:33 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box, Text, Scene, MarkerCamera } from "react-aframe-ar";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2> React AR JS </h2>
+      <div>
+        <a-scene>
+          {/* <a-marker preset="hiro"> */}
+          {/* <a-box
+            position="0 1.5 -3"
+            color="#ff0000"
+            material="opacity:0.5"
+            scale="0.5 0.5 0.5"
+            animation="property:rotation; from:0 0 0; to:0 360 0; dur:2000; loop:true; dir: alternate;"
+          ></a-box> */}
+          {/* </a-marker> */}
+          <a-marker preset="hiro">
+            {/* <a-box
+              color="pink"
+              material="opacity:0.5"
+              // scale="0.5 0.5 0.5"
+              animation={{
+                property: "rotation",
+                from: "0 0 0",
+                to: "0 360 0",
+                dur: 2000,
+                loop: true,
+                dir: "alternate"
+              }} */}
+            {/* // animation="property:rotation; from:0 0 0; to:0 360 0; dur:2000; loop:true; dir: alternate;" */}
+            {/* ></a-box> */}
+          </a-marker>
+          <a-assets>
+            <a-asset-item id="tree" src="./BrainStem.gltf"></a-asset-item>
+          </a-assets>
+          <a-entity gltf-model="##tree"></a-entity>
+          <a-entity camera>
+            <a-cursor></a-cursor>
+          </a-entity>
+        </a-scene>
+      </div>
     </div>
   );
 }
-
-export default App;
